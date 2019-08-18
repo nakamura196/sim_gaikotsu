499fb6627deea2610f9c5fbd8dd7c37b7724a62b
nakamura196 <na.kamura.1263@gmail.com>
2019-08-18 16:37:58 +0900

    update batch

--- bdocker.sh
+++ bdocker.sh
@@ -1,5 +1,5 @@
 git pull
-docker build . -t utda/sim_kunshujo:1.0
-docker stop sim_kunshujo
-docker rm sim_kunshujo
-docker run --name sim_utda -itd -p 8026:5000 utda/sim_kunshujo:1.0
+docker build . -t utda/sim_gaikotsu:1.0
+docker stop sim_gaikotsu
+docker rm sim_gaikotsu
+docker run --name sim_gaikotsu -itd -p 8027:5002 utda/sim_gaikotsu:1.0
